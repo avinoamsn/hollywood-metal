86645db05aa6b175803657ceb203cbf902c50843
Avi Nerenberg <devavmeister@gmail.com>
2019-07-27 16:29:34 -0400

    higher thumbnail img resolution

--- mini-loop.php
+++ mini-loop.php
@@ -2,7 +2,7 @@
 while($query->have_posts()) : $query->the_post(); ?>
 
 	<section class="article">
-		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large'); ?></a>
 		<div class="article__meta">
 			<a class="article__title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 			<a class="article__author" href="<?php the_permalink(); ?>"><?php the_author(); ?></a>
